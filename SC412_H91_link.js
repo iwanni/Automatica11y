425451f95be3714ff1059f102bf17e28815fa1d4
Ikhwan Noor Ikhsan <iwan.ni@rocketmail.com>
2018-03-20 17:38:15 +0700

    solve conflict

--- SC412_H91_link.js
+++ SC412_H91_link.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 //<a>Kalau begini error atau tidak</a>
@@ -13,19 +12,3 @@
             document.getElementsByTagName("a")[i].innerHTML = aText;
         }
     }
-=======
-
-
-//<a>Kalau begini error atau tidak</a>
-    for(let i = 0 ; i < document.getElementsByTagName("a").length ; i++) {
-        if (document.getElementsByTagName("a")[i].getAttribute("href") == false ||
-            document.getElementsByTagName("a")[i].getAttribute("href") == null) {
-            let hrefText = prompt("Please enter href link "+ (i + 1) +":", "");
-            document.getElementsByTagName("a")[i].setAttribute("href", hrefText);
-        }
-        if (document.getElementsByTagName("a")[i].innerHTML == false) {
-            let aText = prompt("Please enter a link text:"+ (i + 1) +":", "");
-            document.getElementsByTagName("a")[i].innerHTML = aText;
-        }
-    }
->>>>>>> 01a3b2756425b178dbd2b8166ab6db0bb4d495a2
